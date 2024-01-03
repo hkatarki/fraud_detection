1af6ea1dc04f0f1bb90775ae3447c8e833c6373a
hkatarki <h.katarki@gmail.com>
2024-01-03 14:43:54 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print ("Adding all ml code here")
+
+def model1 ():
+    print ("adding code for model1")
+
+def model2 ():
+    print ("adding code for model2")
+
+def model3 ():
+    print ("adding code for model3")
+    
